405506a1138e6fee75f38fd3438b1f376d27fb81
Sds Dilgam Mammadov <59636570+sdsdilqem@users.noreply.github.com>
2020-01-08 09:03:20 +0400

    Create decoder.py

---
+++ decoder.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# Small tool to decode ASP.NET __VIEWSTATE variable
+#
+# VIEWSTATE module from https://github.com/yuvadm/viewstate.git
+#
+# spinfoo
+
+import viewstate
+import exceptions
+import sys
+import colors as c
+
+#
+# 08.01.2020
+#
+## Valid format: "/wEPDwU[a-zA-Z0-9=]+"
+
+c.print_red("** ASP.NET __VIEWSTATE decoder **\n")
+c.print_red("** ASP.NET __VIEWSTATE decode edici.  **\n")
+
+if len(sys.argv) != 2:
+    print("Usage / istifadesi:  decoder.py \"<__VIEWSTATE ASP.NET string>\"")
+    print("Example / numune: decoder.py \"/wEwRDwEweWRkoCvssdUOH7PD446REdvEOF6GBCq0=\"")
+    sys.exit()
+
+vst= sys.argv[1]
+
+c.print_blue("[*] Decoding __VIEWSTATE:")
+print(vst)
+try:
+    vs= viewstate.ViewState(vst)
+    c.print_green(str(vs.decode()))
+except exceptions.ViewStateException as e:
+    print (e)
+except IndexError as e:
+    print (e)
